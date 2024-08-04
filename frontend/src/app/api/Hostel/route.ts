b7c5cf2c90cfac4b9e02722020227528d9d31124
Nikhilesh Rana <Nikhileshrana@gmail.com>
2024-08-04 22:41:24 +0530

    Frontend : src:app:api:Hostel: Form Data req Fix

--- frontend/src/app/api/Hostel/route.ts
+++ frontend/src/app/api/Hostel/route.ts
@@ -4,14 +4,27 @@ import axios from 'axios';
 export async function POST(req: NextRequest) {
   try {
     const formData = await req.json();
+    
+    if (!formData) {
+      return NextResponse.json({ error: 'No data provided' }, { status: 400 });
+    }
+    console.log(formData);
+
     const response = await axios.post(`${process.env.BASEURL}api/hostel/`, formData);
 
     return NextResponse.json({ message: 'Successfully Registered', data: response.data });
+
   } catch (error) {
+    // Handle errors from the external API
     const output = (error as any).response?.data;
 
-    return NextResponse.json({ 
-      error: `${output[Object.keys(output)[0]][0]}`, 
-    }, { status: 400 });
+    if (output) {
+      return NextResponse.json({ 
+        error: `${output[Object.keys(output)[0]][0]}`, 
+      }, { status: 400 });
+    }
+
+    // General error fallback
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
   }
 }
