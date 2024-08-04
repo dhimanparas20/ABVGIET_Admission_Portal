bcd8daf581c4b44d2281915a975f01c77d3ae45f
dhimanparas20 <dhimanparas20@gmail.com>
2024-08-04 21:56:42 +0530

    Dockerfile: accurate containers
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,36 +4,64 @@ from .models import Student,HostelApplication,Form
 from .serializers import StudentSerializer,HostelApplicationSerializer,FormSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions, status
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    permission_classes = [permissions.AllowAny]  # Set appropriate permissions
-    http_method_names = ['post']  # Only allow POST
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['post']
 
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['branch', 'category', 'subcategory', 'round']
     ordering_fields = ['first_name', 'last_name', 'percentage_scored']
     search_fields = ['first_name', 'last_name', 'email', 'aadhaar_no']
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class HostelApplicationViewSet(viewsets.ModelViewSet):
     queryset = HostelApplication.objects.all()
     serializer_class = HostelApplicationSerializer
-    permission_classes = [permissions.AllowAny]  # Set appropriate permissions
-    http_method_names = ['post']  # Only allow POST
+    permission_classes = [permissions.AllowAny]
 
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['branch', 'category', 'department', 'semester_year']
     ordering_fields = ['applicant_full_name', 'branch', 'category', 'department', 'semester_year']
     search_fields = ['applicant_full_name', 'branch', 'category', 'department', 'semester_year']
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class FormViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
-    permission_classes = [permissions.AllowAny]  # Set appropriate permissions
-    http_method_names = ['post']  # Only allow POST
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['post']
 
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['name', 'phone_number']
     ordering_fields = ['name', 'phone_number']
-    search_fields = ['name', 'phone_number']   
+    search_fields = ['name', 'phone_number']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
